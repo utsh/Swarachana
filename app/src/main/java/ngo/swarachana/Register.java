080e39542186d4e20d3865e8521b1fd00e06e86b
utsh <longliveutkarsh@gmail.com>
2016-02-14 22:27:14 +0530

    Completed registration page layout

--- app/src/main/java/ngo/swarachana/Register.java
+++ app/src/main/java/ngo/swarachana/Register.java
@@ -13,7 +13,11 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
